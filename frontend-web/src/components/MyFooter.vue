9819b5549c547689cd5774329ef6fea4ddf20965
alexandrupop <alexandrupop@ucs.ro>
2022-04-27 18:22:41 +0300

    Modified footer

--- frontend-web/src/components/MyFooter.vue
+++ frontend-web/src/components/MyFooter.vue
@@ -6,6 +6,8 @@
 	>
 		<b-navbar-brand>
 			<div style="font-size: 25px">Proiect realizat de:</div>
+			<div style="text-align: center; font-size: small">Lupu Andy Constantin (1110)</div>
+			<div style="text-align: center; font-size: small">Orzan Teodora Diana (1111)</div>
 			<div style="text-align: center; font-size: small">Pop Alexandru (1111)</div>
 		</b-navbar-brand>
 	</b-navbar>
